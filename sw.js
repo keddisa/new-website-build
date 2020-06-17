3c9d4e8476ec2d732b84836f242265f83af33cb6
keddisa <keddisa@gmail.com>
2020-06-17 18:19:54 -0400

    capitalize asset imports in components

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e6af5adbc69afa56d168.js"
+    "url": "webpack-runtime-abb29cfb3a99aa5955c5.js"
   },
   {
     "url": "framework-5fc6169171e885c3b522.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f93e48c7265dd48f119e7e258180b41f"
+    "revision": "6c6f2f0c9ed3ca7c3ec46442070ee385"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "3aef1a95089d7eb46a981f0247a0ed88"
+    "revision": "5702cec9d96b552869c816251506980c"
   },
   {
     "url": "manifest.webmanifest",
